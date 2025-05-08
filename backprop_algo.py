673f10438c6193429ea632bb72b41447967b8c6b
Boke Lets'oara <bokeletsoara@gmail.com>
2025-05-08 15:10:55 +0200

    backpropagation function

--- backprop_algo.py
+++ backprop_algo.py
@@ -44,3 +44,19 @@ def feedforward(X, W1, b1, W2, b2):
     A2 = sigmoid(Z2)
 
     return Z1, A1, Z2, A2
+
+def backpropagation(X, y, Z1, A1, Z2, A2, W2):
+    m = X.shape[0]  # number of samples
+
+    # Output error
+    dZ2 = (A2 - y) * sigmoid_derivative(Z2)
+    dW2 = np.dot(A1.T, dZ2) / m
+    db2 = np.sum(dZ2, axis=0, keepdims=True) / m
+
+    # Hidden layer error
+    dZ1 = np.dot(dZ2, W2.T) * sigmoid_derivative(Z1)
+    dW1 = np.dot(X.T, dZ1) / m
+    db1 = np.sum(dZ1, axis=0, keepdims=True) / m
+
+    return dW1, db1, dW2, db2
+
