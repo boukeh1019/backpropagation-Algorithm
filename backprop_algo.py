c8c9150875287d844bb78124baa810092d54676b
Boke Lets'oara <bokeletsoara@gmail.com>
2025-05-08 15:08:14 +0200

    sigmoid and its derivative

--- backprop_algo.py
+++ backprop_algo.py
@@ -17,3 +17,11 @@ X = (X - X.mean(axis=0)) / X.std(axis=0)
 
 # Train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def sigmoid_derivative(z):
+    s = sigmoid(z)
+    return s * (1 - s)
+
